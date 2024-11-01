c83b4931404a76fe73eca48d926f6851f8a21b93
lyy0323 <lyy0323@sjtu.edu.cn>
2024-11-01 16:42:14 +0800

    first commit

--- assets/js/tongji.baidu.js
+++ assets/js/tongji.baidu.js
@@ -1,7 +1,7 @@
 var _hmt = _hmt || [];
 (function() {
   var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?ec7470452f344e57c8085be5be58583f";
+  hm.src = "https://hm.baidu.com/hm.js?9ed401402957873a3adaf52e4253fe75";
   var s = document.getElementsByTagName("script")[0];
   s.parentNode.insertBefore(hm, s);
 })();
